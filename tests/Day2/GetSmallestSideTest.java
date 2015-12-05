698b65d9480e4e4d4958bf4a9540548d47a7e059
D.Burnley <db458@kent.ac.uk>
2015-12-05 14:45:05 +0000

    get smallest side

--- tests/Day2/GetSmallestSideTest.java
+++ tests/Day2/GetSmallestSideTest.java
@@ -15,8 +15,8 @@ public class GetSmallestSideTest {
     int width;
     int height;
 
-    private void assertSmallestSide(int length) {
-        assertEquals(1, getSmallest.getSmallestSide(length, width, height));
+    private void assertSmallestSide(int expectedArea) {
+        assertEquals(expectedArea, getSmallest.getSmallestSide(length, width, height));
     }
 
     @Before
@@ -25,11 +25,56 @@ public class GetSmallestSideTest {
     }
 
     @Test
-    public void givenSidesAllOfLengthOne_whenGettingSmallestSize_returnOne() throws Exception {
+    public void givenSidesAllOfLengthOne_whenGettingSmallestSize_returnOne() {
         length = 1;
         width = 1;
         height = 1;
 
         assertSmallestSide(1);
+    }
+
+    @Test
+    public void GivenAllButOneSideIsOfLengthOne_WhenGettingSmallestSize_ThenReturnOne() {
+        length = 1;
+        width = 1;
+        height = 10;
+
+        assertSmallestSide(1);
+    }
+
+    @Test
+    public void GivenOneLengthOneAndOtherSizesTen_WhenGettingSmallestSize_ThenReturnTen() {
+        length = 1;
+        width = 10;
+        height = 10;
+
+        assertSmallestSide(10);
+    }
+
+    @Test
+    public void GivenLengthAndWidthSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnLengthTimesWidth() {
+        length = 1;
+        width = 2;
+        height = 10;
+
+        assertSmallestSide(length * width);
+    }
+
+    @Test
+    public void GivenLengthAndHeightSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnLengthTimesHeight() {
+        length = 1;
+        width = 10;
+        height = 2;
+
+        assertSmallestSide(length * height);
+    }
+
+    @Test
+    public void GivenWidthAndHeightSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnWidthTimesHeight() {
+        length = 10;
+        width = 1;
+        height = 2;
+
+        assertSmallestSide(width * height);
     }
 }
