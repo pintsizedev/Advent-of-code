c69b7ad0e8cbf963feb6d1f4dfc81ce623d1ddac
D.Burnley <db458@kent.ac.uk>
2015-12-05 14:55:50 +0000

    refactor

--- tests/Day2/GetSmallestSideTest.java
+++ tests/Day2/GetSmallestSideTest.java
@@ -8,12 +8,9 @@ import static org.junit.Assert.*;
 /**
  * Created by pintsizedev
  */
-public class GetSmallestSideTest {
+public class GetSmallestSideTest extends AbstractPresentTest {
 
     Day2.UseCase.GetSmallestSide getSmallest;
-    int length;
-    int width;
-    int height;
 
     private void assertSmallestSide(int expectedArea) {
         assertEquals(expectedArea, getSmallest.getSmallestSide(length, width, height));
@@ -26,55 +23,37 @@ public class GetSmallestSideTest {
 
     @Test
     public void givenSidesAllOfLengthOne_whenGettingSmallestSize_returnOne() {
-        length = 1;
-        width = 1;
-        height = 1;
-
+        this.setSizes(1, 1, 1);
         assertSmallestSide(1);
     }
 
     @Test
     public void GivenAllButOneSideIsOfLengthOne_WhenGettingSmallestSize_ThenReturnOne() {
-        length = 1;
-        width = 1;
-        height = 10;
-
+        this.setSizes(1,1,10);
         assertSmallestSide(1);
     }
 
     @Test
     public void GivenOneLengthOneAndOtherSizesTen_WhenGettingSmallestSize_ThenReturnTen() {
-        length = 1;
-        width = 10;
-        height = 10;
-
+        this.setSizes(1, 10, 10);
         assertSmallestSide(10);
     }
 
     @Test
     public void GivenLengthAndWidthSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnLengthTimesWidth() {
-        length = 1;
-        width = 2;
-        height = 10;
-
+        this.setSizes(1, 2, 10);
         assertSmallestSide(length * width);
     }
 
     @Test
     public void GivenLengthAndHeightSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnLengthTimesHeight() {
-        length = 1;
-        width = 10;
-        height = 2;
-
+        this.setSizes(1, 10, 2);
         assertSmallestSide(length * height);
     }
 
     @Test
     public void GivenWidthAndHeightSmallestAndBothDifferent_WhenGettingSmallestSize_ThenReturnWidthTimesHeight() {
-        length = 10;
-        width = 1;
-        height = 2;
-
+        this.setSizes(10, 1, 2);
         assertSmallestSide(width * height);
     }
 }
